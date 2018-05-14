3e2023afdc3ed8920f9046fca2218a16562b126c
Mike <matroxmike@icloud.com>
2018-05-14 15:14:32 +1000

    beginning to be useful

--- xmlRunMe.sh
+++ xmlRunMe.sh
@@ -4,6 +4,9 @@ xml2-config --cflags
 xml2-config --libs
 
 
-#gcc -o iol `xml2-config --cflags` iol.c `xml2-config --libs`
+#gcc -o io1 `xml2-config --cflags` iol.c `xml2-config --libs`
 
 gcc -o reader2 `xml2-config --cflags` reader2.c `xml2-config --libs`
+
+gcc -o io1 io1.c `xml2-config --cflags` `xml2-config --libs`
+
