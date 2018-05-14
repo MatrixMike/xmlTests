e11ee3d449b75111454a3f0077d1115488d5b576
Mike <matroxmike@icloud.com>
2018-05-14 14:57:43 +1000

    xml experiments2

--- xmlRunMe.sh
+++ xmlRunMe.sh
@@ -4,6 +4,6 @@ xml2-config --cflags
 xml2-config --libs
 
 
-gcc -o example `xml2-config --cflags` iol.c `xml2-config --libs`
+#gcc -o iol `xml2-config --cflags` iol.c `xml2-config --libs`
 
-gcc -o example `xml2-config --cflags` reader2.c `xml2-config --libs`
+gcc -o reader2 `xml2-config --cflags` reader2.c `xml2-config --libs`
