323ff59067cd169f9e13fcf43cb3803832f2be29
Mike <matroxmike@icloud.com>
2018-05-14 17:34:24 +1000

    forgotten files

--- xmlRunMe.sh
+++ xmlRunMe.sh
@@ -12,4 +12,5 @@ gcc -o io1 io1.c `xml2-config --cflags` `xml2-config --libs`
 
 gcc -o io2 io2.c `xml2-config --cflags` `xml2-config --libs`  -DLIBXML_TREE_ENABLED  -DLIBXML_OUTPUT_ENABLED
 
+gcc -o tree2 tree2.c `xml2-config --cflags` `xml2-config --libs`  -DLIBXML_TREE_ENABLED  -DLIBXML_OUTPUT_ENABLED
 
